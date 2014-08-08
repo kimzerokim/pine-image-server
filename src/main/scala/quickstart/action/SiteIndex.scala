d44d748ef4ff1aaae73f11a08b1acb848c5a19ed
reaperes <emerald105@hanmail.net>
2014-08-08 21:54:46 +0900

    complete skeleton code

--- src/main/scala/quickstart/action/SiteIndex.scala
+++ src/main/scala/quickstart/action/SiteIndex.scala
@@ -1,10 +1,37 @@
 package quickstart.action
 
-import xitrum.annotation.GET
+import java.io.File
+import java.nio.file.{Paths, Files}
 
-@GET("")
-class SiteIndex extends DefaultLayout {
+import io.netty.handler.codec.http.multipart.FileUpload
+
+import xitrum.{SkipCsrfCheck, Action}
+import xitrum.annotation.{POST, GET}
+
+@GET("upload/:name")
+class Upload extends Action with SkipCsrfCheck {
   def execute() {
-    respondView()
+    val name = param("name")
+    val existTrue = Files.exists(Paths.get("/Users/Andy/"+name))
+    if(existTrue)
+      respondFile("/Users/Andy/"+name)
+    else
+      respondJson("not pine")
+  }
+}
+
+@POST("upload")
+class HelloAction extends Action with SkipCsrfCheck {
+  def execute() {
+    paramo[FileUpload]("file") match {
+      case Some(file) =>
+        val file = param[FileUpload]("file")
+        val path = "/Users/Andy/" + file.getFilename
+        file.renameTo(new File(path))
+
+      case None =>
+        flash("Please upload a nonempty file")
+    }
+    respondJson("pine")
   }
 }
